2f848cc35d0fd4d0219ac16d164b2c6ea4da3590
axiang <wyxworkmail@163.com>
2020-05-02 13:22:31 +0800

    init project

--- CNTextSim.py
+++ CNTextSim.py
@@ -20,69 +20,70 @@ def get_cos(x, y):
     return cos1 / (cos21 * cos22)
 
 
-startTime = time.time()
-testFilePath = sys.argv[1]
-sampleFilePath = sys.argv[2]
-resultFilePath = sys.argv[3]
+if __name__ == '__main__':
+    startTime = time.time()
+    testFilePath = sys.argv[1]
+    sampleFilePath = sys.argv[2]
+    resultFilePath = sys.argv[3]
 
-f1 = codecs.open(sampleFilePath, "r", "utf-8")
-try:
-    f1_text = f1.read()
-finally:
-    f1.close()
+    f1 = codecs.open(sampleFilePath, "r", "utf-8")
+    try:
+        f1_text = f1.read()
+    finally:
+        f1.close()
 
-f1_seg_list = jieba.cut(f1_text)
-# first test
-ftest1 = codecs.open(testFilePath, "r", "utf-8")
-try:
-    ftest1_text = ftest1.read()
-finally:
-    ftest1.close()
-ftest1_seg_list = jieba.cut(ftest1_text)
+    f1_seg_list = jieba.cut(f1_text)
+    # first test
+    ftest1 = codecs.open(testFilePath, "r", "utf-8")
+    try:
+        ftest1_text = ftest1.read()
+    finally:
+        ftest1.close()
+    ftest1_seg_list = jieba.cut(ftest1_text)
 
-# read sample text
-# remove stop word and constructor dict
-f_stop = codecs.open(r"D:\testfile\stopwords.txt", "r", "utf-8")
-try:
-    f_stop_text = f_stop.read()
-finally:
-    f_stop.close()
-f_stop_seg_list = f_stop_text.split("\n")
+    # read sample text
+    # remove stop word and constructor dict
+    f_stop = codecs.open(r"D:\testfile\stopwords.txt", "r", "utf-8")
+    try:
+        f_stop_text = f_stop.read()
+    finally:
+        f_stop.close()
+    f_stop_seg_list = f_stop_text.split("\n")
 
-test_words = {}
-all_words = {}
+    test_words = {}
+    all_words = {}
 
-for word in f1_seg_list:
-    if not (word.strip()) in f_stop_seg_list:
-        test_words.setdefault(word, 0)
-        all_words.setdefault(word, 0)
-        all_words[word] += 1
+    for word in f1_seg_list:
+        if not (word.strip()) in f_stop_seg_list:
+            test_words.setdefault(word, 0)
+            all_words.setdefault(word, 0)
+            all_words[word] += 1
 
-    # read to be tested word
-mytest1_words = copy.deepcopy(test_words)
-for word in ftest1_seg_list:
-    if not (word.strip()) in f_stop_seg_list:
-        if word in mytest1_words:
-            mytest1_words[word] += 1
+        # read to be tested word
+    mytest1_words = copy.deepcopy(test_words)
+    for word in ftest1_seg_list:
+        if not (word.strip()) in f_stop_seg_list:
+            if word in mytest1_words:
+                mytest1_words[word] += 1
 
-# calculate sample with to be tested text sample
-sampleData = []
-testFileData = []
-for key in all_words.keys():
-    sampleData.append(all_words[key])
-    testFileData.append(mytest1_words[key])
-testFileSim = get_cos(sampleData, testFileData)
+    # calculate sample with to be tested text sample
+    sampleData = []
+    testFileData = []
+    for key in all_words.keys():
+        sampleData.append(all_words[key])
+        testFileData.append(mytest1_words[key])
+    testFileSim = get_cos(sampleData, testFileData)
 
-resultJson = json.dumps({
-    "测试文件路径": testFilePath,
-    "样本文件路径": sampleFilePath,
-    "结果文件路径": resultFilePath,
-    "余弦相似度": testFileSim,
-    "测试开始时间": startTime,
-    "测试结束时间": time.time()
-}, ensure_ascii=False)
+    resultJson = json.dumps({
+        "测试文件路径": testFilePath,
+        "样本文件路径": sampleFilePath,
+        "结果文件路径": resultFilePath,
+        "余弦相似度": testFileSim,
+        "测试开始时间": startTime,
+        "测试结束时间": time.time()
+    }, ensure_ascii=False)
 
-print(resultJson)
-resultFile = codecs.open(resultFilePath, "w", "utf-8")
-resultFile.write(resultJson)
-resultFile.close()
+    print(resultJson)
+    resultFile = codecs.open(resultFilePath, "w", "utf-8")
+    resultFile.write(resultJson)
+    resultFile.close()
